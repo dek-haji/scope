149f4b95dd34614fd313d9465524023f492c7fba
dek <deeeqhaji@gmail.com>
2018-12-04 13:24:30 -0600

    i created one repo for all the js files

---
+++ conjuction.js
@@ -0,0 +1,8 @@
+let conjunction = function (firstWord, secondWord) {
+    let conjoinedWord = `${firstWord}, ${secondWord}`
+
+    console.log(conjoinedWord)
+   
+}
+
+conjunction("Master", "Card")
